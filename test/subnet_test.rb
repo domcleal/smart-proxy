0f88f09ec909c43eda66e1773f2cde5aa1ada5b7
Dominic Cleal <dcleal@redhat.com>
2014-04-09 17:21:52 +0100

    fix test

--- test/subnet_test.rb
+++ test/subnet_test.rb
@@ -117,11 +117,15 @@ class Proxy::DHCPSubnetTest < Test::Unit::TestCase
 
   def test_should_reuse_ip_if_from_same_subnet
     @subnet.stubs(:has_mac?).returns(stub(:ip => '192.168.0.10'))
+    @subnet.stubs(:icmp_pingable?)
+    @subnet.stubs(:tcp_pingable?)
     assert_equal '192.168.0.10', @subnet.unused_ip({:mac => '0'})
   end
 
   def test_should_not_reuse_ip_if_from_other_subnet
     @subnet.stubs(:has_mac?).returns(stub(:ip => '10.0.0.10'))
+    @subnet.stubs(:icmp_pingable?)
+    @subnet.stubs(:tcp_pingable?)
     assert_equal false, @subnet.unused_ip({:mac => '0'}).include?("10.0.0")
   end
 
